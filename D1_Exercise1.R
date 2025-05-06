0229da629c706cca822f7458a39779e5cd91ceba
danielojnorth <danielojnorth@gmail.com>
2025-05-06 15:58:22 +0100

    Adding R scripts generated on week 1 day 1 of OBDS R course

---
+++ D1_Exercise1.R
@@ -0,0 +1,18 @@
+##First exercise 
+
+#Assign values 1 to 200 to a vector named a
+a <- 1:100
+
+
+b <- a*123
+b
+b[44] 
+
+
+m <- c('actb', 100, 3.4)
+m[2]
+m[2]*4
+
+D <- c('purple', 'blue', 'yellow', 'red') 
+names(D) <- c("colour_1", "colour_2", "colour_3", "colour_4")
+D
